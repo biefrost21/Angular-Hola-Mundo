18dba806c554a2a006808b55f01f6bfa948d7776
Fede <biefrost21@gmail.com>
2020-08-17 14:32:10 -0300

    Agrego Proyecto Hola Mundo

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-body',
+    templateUrl: `./body.component.html`
+})
+
+export class BodyComponent{
+
+    mostrar = 'true';
+    frase:any = {
+        mensaje: 'Un gran poder conlleva una gran Responsabilidad',
+        autor: 'Ben Parker'
+    };
+
+    personajes: string[] = ['Spiderman', 'Venom', 'Iroman', 'Capitan America'];
+   
+}
